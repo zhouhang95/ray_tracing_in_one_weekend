c3888d40dbc51b5ad549e1dac1e132e5ce1e3739
zhouhang95 <765229842@qq.com>
2022-10-23 02:24:14 +0800

    RoughPlastic

--- src/pbr.rs
+++ src/pbr.rs
@@ -91,6 +91,95 @@ fn smith_geo_ggx(n_dot_v: f32, alpha: f32) -> f32 {
     1. / (n_dot_v + (a + b - a * b).sqrt())
 }
 
+/// Fresnel equation of a dielectric interface.
+/// https://seblagarde.wordpress.com/2013/04/29/memo-on-fresnel-equations/
+/// n_dot_i: abs(cos(incident angle))
+/// n_dot_t: abs(cos(transmission angle))
+/// eta: eta_transmission / eta_incident
+pub fn fresnel_dielectric(n_dot_i: f32, n_dot_t: f32, eta: f32) -> f32 {
+    assert!(n_dot_i >= 0. && n_dot_t >= 0. && eta > 0.);
+    let rs = (n_dot_i - eta * n_dot_t) / (n_dot_i + eta * n_dot_t);
+    let rp = (eta * n_dot_i - n_dot_t) / (eta * n_dot_i + n_dot_t);
+    let f = (rs * rs + rp * rp) / 2.;
+    f
+}
+
+/// https://seblagarde.wordpress.com/2013/04/29/memo-on-fresnel-equations/
+/// This is a specialized version for the code above, only using the incident angle.
+/// The transmission angle is derived from
+/// n_dot_i: cos(incident angle) (can be negative)
+/// eta: eta_transmission / eta_incident
+pub fn fresnel_dielectric_2(n_dot_i: f32, eta: f32) -> f32 {
+    assert!(eta > 0.);
+    let n_dot_t_sq = 1. - (1. - n_dot_i * n_dot_i) / (eta * eta);
+    if n_dot_t_sq < 0. {
+        // total internal reflection
+        return 1.;
+    }
+    let n_dot_t = n_dot_t_sq.sqrt();
+    fresnel_dielectric(n_dot_i.abs(), n_dot_t, eta)
+}
+
+/// The masking term models the occlusion between the small mirrors of the microfacet models.
+/// See Eric Heitz's paper "Understanding the Masking-Shadowing Function in Microfacet-Based BRDFs"
+/// for a great explanation.
+/// https://jcgt.org/published/0003/02/03/paper.pdf
+/// The derivation is based on Smith's paper "Geometrical shadowing of a random rough surface".
+/// Note that different microfacet distributions have different masking terms.
+pub fn smith_masking_gtr2(v_local: Vec3A, roughness: f32) -> f32{
+    let alpha = roughness * roughness;
+    let a2 = alpha * alpha;
+    let v2 = v_local * v_local;
+    let lambda = (-1. + (1. + a2 * (v2.x + v2.y) / v2.z).sqrt()) / 2.;
+    1. / (1. + lambda)
+}
+
+pub fn smith_masking_gtr2_2(v_world: Vec3A, n: Vec3A, roughness: f32) -> f32{
+    let alpha = roughness * roughness;
+    let a2 = alpha * alpha;
+    let v2_z_ = v_world.dot(n);
+    let v2_z = v2_z_ * v2_z_;
+    let lambda = (-1. + (1. + a2 * (1. - v2_z) / v2_z).sqrt()) / 2.;
+    1. / (1. + lambda)
+}
+pub struct RoughPlastic {
+    pub spec_color: Arc<dyn Texture>,
+    pub diff_color: Arc<dyn Texture>,
+    pub roughness: f32,
+    pub eta: f32,
+}
+
+impl Material for RoughPlastic {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: &mut Vec3A, scattered: &mut Ray) -> bool {
+        let p = offset_hit_point(rec.p, rec.norm);
+        let dir_o = random_on_hemisphere(rec.norm);
+        let n_dot_i = rec.norm.dot(-r_in.d);
+        let n_dot_o = rec.norm.dot(dir_o);
+        let h = (dir_o - r_in.d).normalize();
+        let h_dot_i = h.dot(-r_in.d);
+        let h_dot_o = h.dot(dir_o);
+        let n_dot_h = rec.norm.dot(h);
+
+
+        let kd = self.diff_color.value(rec.uv, rec.p);
+        let ks = self.spec_color.value(rec.uv, rec.p);
+
+        let roughness = self.roughness.clamp(0.01, 1.);
+        let f_o = fresnel_dielectric_2(h_dot_o, self.eta);
+        let d = gtr2(n_dot_h, roughness);
+        let g = smith_masking_gtr2_2(-r_in.d, rec.norm, roughness)
+                    *smith_masking_gtr2_2(dir_o, rec.norm, roughness);
+        let spec_contrib = ks * (g * f_o * d) / (4. * n_dot_i * n_dot_o);
+
+        let f_i = fresnel_dielectric_2(h_dot_i, self.eta);
+        let diff_contrib = kd * (1. - f_o) * (1. - f_i) * FRAC_1_PI;
+
+        *scattered = Ray {o: p, d: dir_o, s: r_in.s};
+        *attenuation = (spec_contrib + diff_contrib) * n_dot_o * 2. * PI;
+        true
+    }
+}
+
 
 pub struct Clearcoat {
     pub clearcoat_gloss: f32,
